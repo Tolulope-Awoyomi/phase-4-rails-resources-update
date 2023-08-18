3a5e30719571b8fa134b49a3ec359259fbc5aa03
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-18 18:45:55 -0400

    Code lab solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+  # create a custom route to take the work of calculating the number of likes and incrementing it off the frontend. All the frontend would do is to send a arequenst to our new custom  route.
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
